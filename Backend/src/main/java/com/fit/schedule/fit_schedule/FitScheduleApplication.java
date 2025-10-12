0d8e3b0df6004f570147d5468290ed647a53d35c
Martin Grahovski <martingrahovski2004@gmail.com>
2025-10-12 22:16:09 +0300

    Initial

---
+++ Backend/src/main/java/com/fit/schedule/fit_schedule/FitScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.fit.schedule.fit_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitScheduleApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/fit/schedule/fit_schedule/FitScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.schedule.fit_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
