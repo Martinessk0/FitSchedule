17440b05c5365d32d3fdcea341c4523f75fe3e30
Martin Grahovski <martingrahovski2004@gmail.com>
2026-01-16 23:16:04 +0200

    Commenting MmaGymApplicationTests.java for test purposes

--- Backend/src/test/java/com/mmagym/MmaGymApplicationTests.java
+++ Backend/src/test/java/com/mmagym/MmaGymApplicationTests.java
@@ -1,13 +1,13 @@
-package com.mmagym;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MmaGymApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.mmagym;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MmaGymApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
